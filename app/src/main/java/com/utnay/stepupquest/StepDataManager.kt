d2dbaa24156028599a43df2ac83e9bad0a158dc9
Nxxil <rfantonio53@gmail.com>
2025-07-27 23:07:47 -0700

    Implementación de notificaciones en el dispositivo Wear e integración básica con el dispositivo móvil

--- app/src/main/java/com/utnay/stepupquest/StepDataManager.kt
+++ app/src/main/java/com/utnay/stepupquest/StepDataManager.kt
@@ -2,6 +2,7 @@ package com.utnay.stepupquest
 
 import android.content.Context
 import android.util.Log
+import com.google.android.gms.wearable.CapabilityClient
 import com.google.android.gms.wearable.PutDataMapRequest
 import com.google.android.gms.wearable.PutDataRequest
 import com.google.android.gms.wearable.Wearable
@@ -58,3 +59,33 @@ class StepDataManager(private val context: Context) {
         }
     }
 }
+
+// class StepDataManager(private val context: Context) {
+//
+// private val messageClient by lazy { Wearable.getMessageClient(context) }
+// private val capabilityClient by lazy { Wearable.getCapabilityClient(context) }
+//
+// fun sendNotificationToWear(percentage: Int, messageText: String) {
+// val messageData = "$percentage;$messageText".toByteArray()
+// // Find connected nodes that have the "wear_app" capability (optional, but good practice)
+// capabilityClient.getCapability("wear_app", CapabilityClient.FILTER_REACHABLE)
+// .addOnSuccessListener { capabilityInfo ->
+// val nodes = capabilityInfo.nodes
+// nodes.forEach { node ->
+// messageClient.sendMessage(node.id, "/step_milestone", messageData)
+// .addOnSuccessListener {
+// Log.d("StepDataManager", "Message sent to ${node.displayName}")
+// }
+// .addOnFailureListener {
+// Log.e("StepDataManager", "Failed to send message to ${node.displayName}", it)
+// }
+// }
+// if (nodes.isEmpty()) {
+// Log.w("StepDataManager", "No wearable nodes found with 'wear_app' capability")
+// }
+// }
+// .addOnFailureListener {
+// Log.e("StepDataManager", "Failed to get capability info", it)
+// }
+// }
+// }
